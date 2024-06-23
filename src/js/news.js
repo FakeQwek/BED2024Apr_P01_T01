6593afd456e430aa30c8c9d00840fc048472ebeb
FakeQwek <133119964+FakeQwek@users.noreply.github.com>
2024-06-23 15:30:26 +0800

    Implemented load button
    Allows user to see more news posts

--- src/js/news.js
+++ src/js/news.js
@@ -1,33 +1,26 @@
 apikey = "ad61a3b55ab20ed21479950c798b39d9";
 url = 'https://gnews.io/api/v4/top-headlines?lang=en&country=sg&max=10&apikey=' + apikey;
 const newsContainer = document.getElementById("news-container");
+const loadButton = document.getElementById("load-button");
+const loadContainer = document.getElementById("load-container");
+
 //Gets and adds initial news posts
 getAllNews(url);
 storednewsData = [];
 //lastIndex remembers where to start loading news posts
 var lastIndex = 0;
-
-console.log(url);
-//Will update the database with new news responses from api every launch
-getNewsResponse(url);
-
-
-
-
-
-
-function populateNews(news, newsContainer) {
-  {
-    console.log(news);
+loadButton.addEventListener("click", function (){
+    
     for (i = 0; i < 5; i++){
 
         newsIndex = lastIndex + i;
-        if (newsIndex >= news.length) {
+        if (newsIndex >= storednewsData.length) {
             console.log("No more news to populate!");
-            return;
+            loadButton.innerHTML = `End of News`
+            break;
         }
-        article = news[newsIndex];
-        newsContainer.insertAdjacentHTML("beforeend",
+        article = storednewsData[newsIndex];
+        loadContainer.insertAdjacentHTML("beforebegin",
             `<div class="flex justify-center w-full">
                 <div class="card w-5/6 bg-white news-card">
                     <div class="news-body">
@@ -41,7 +34,71 @@ function populateNews(news, newsContainer) {
         );
     }
     lastIndex += 5;
-  }
+
+});
+console.log(url);
+//Will update the database with new news responses from api every launch
+getNewsResponse(url);
+
+
+
+
+function populateNewNews(storednewsData, loadButton) {
+    
+        console.log("Clicked on load");
+        for (i = 0; i < 5; i++){
+    
+            newsIndex = lastIndex + i;
+            if (newsIndex >= storednewsData.length) {
+                
+                return;
+            }
+            article = storednewsData[newsIndex];
+            loadButton.insertAdjacentHTML("beforebegin",
+                `<div class="flex justify-center w-full">
+                    <div class="card w-5/6 bg-white news-card">
+                        <div class="news-body">
+                            <h3 class="news-source">from ${article["newsSource"]}</h3>
+                            <h2 class="card-title">${article["newsId"]}</h2>
+                            <img class="news-thumbnail object-scale-down" src="${article["newsImage"]}" >
+                            <p>${article["newsDesc"]}</p>
+                        </div>
+                    </div>
+                </div>` 
+            );
+        }
+        lastIndex += 5;
+      
+}
+
+
+
+function populateNews(news, newsContainer) {
+  
+    console.log(news);
+    for (i = 0; i < 5; i++){
+
+        newsIndex = lastIndex + i;
+        if (newsIndex >= news.length) {
+            console.log("No more news to populate!");
+            return;
+        }
+        article = news[newsIndex];
+        newsContainer.insertAdjacentHTML("afterbegin",
+            `<div class="flex justify-center w-full">
+                <div class="card w-5/6 bg-white news-card">
+                    <div class="news-body">
+                        <h3 class="news-source">from ${article["newsSource"]}</h3>
+                        <h2 class="card-title">${article["newsId"]}</h2>
+                        <img class="news-thumbnail object-scale-down" src="${article["newsImage"]}" >
+                        <p>${article["newsDesc"]}</p>
+                    </div>
+                </div>
+            </div>` 
+        );
+    }
+    lastIndex += 5;
+  
 }
 
 //Removes illegal sql characters
