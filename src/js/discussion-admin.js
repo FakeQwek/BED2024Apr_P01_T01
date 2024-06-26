fd3caa195b71bdab53c2fd3a9d962a425875e265
mzul1 <s10255622@connect.np.edu.sg>
2024-06-27 00:23:34 +0800

    added reported posts and banned users page

--- src/js/discussion-admin.js
+++ src/js/discussion-admin.js
@@ -1,18 +1,29 @@
-
 const popupButton = document.getElementById('popupButton');
 const popupDialog = document.getElementById('popupDialog');
 const cancelButton = document.getElementById('cancelButton');
+const editDetailsButton = document.getElementById('editDetailsButton');
+const editDialog = document.getElementById('editDialog');
+const cancelEditButton = document.getElementById('cancelEditButton');
 
 popupButton.addEventListener('click', () => {
     popupDialog.showModal();
 });
 
-cancelButton.addEventListener('click', () => {
+editDetailsButton.addEventListener('click', () => {
     popupDialog.close();
+    editDialog.showModal();
+});
+
+cancelEditButton.addEventListener('click', () => {
+    editDialog.close();
+    popupDialog.showModal();
 });
 
 window.addEventListener('click', (event) => {
     if (event.target === popupDialog) {
         popupDialog.close();
+    }
+    if (event.target === editDialog) {
+        editDialog.close();
     }
 });
